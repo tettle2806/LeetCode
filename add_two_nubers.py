018f0c36b23482af15eb5c22272db69a31260a7c
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-03 11:16:10 +0500

    add two numbers solution

--- add_two_nubers.py
+++ add_two_nubers.py
@@ -3,19 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from typing import Optional
-
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = int(''.join(map(str, l1)))
-        num2 = int(''.join(map(str, l2)))
-        result = []
-        x = num1 + num2
-        while x > 0:
-            result.append(x % 10)
-            x //= 10
+        dummy = ListNode()
+        cur = dummy
 
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        print(result)
-        return result
+            val = v1 + v2 + carry
+
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
