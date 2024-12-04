c3cdaa65ad351da28ba0a15c2946ab6a259cf39b
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2024-12-04 11:47:23 +0500

    Solution Roman to integer

--- add_two_nubers.py
+++ add_two_nubers.py
@@ -26,3 +26,27 @@ class Solution:
             l2 = l2.next if l2 else None
 
         return dummy.next
+
+"""
+Roman to integer
+"""
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_numerals = {"I": 1,
+                          "V": 5,
+                          "X": 10,
+                          "L": 50,
+                          "C": 100,
+                          "D": 500,
+                          "M": 1000
+                          }
+        int_value = 0
+        for i in range(len(s)):
+            if s[i] in roman_numerals:
+                if i + 1 < len(s) and roman_numerals[s[i]] < roman_numerals[s[i + 1]]:
+                    int_value -= roman_numerals[s[i]]
+                else:
+                    int_value += roman_numerals[s[i]]
+            else:
+                break
+        return int_value
